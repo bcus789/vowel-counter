8dff89dec1a72a528c3f9f4f374aa41eec0359b1
Bonello <Geno.Bonello@treasurer.ca.gov>
2023-06-16 14:56:52 -0700

    finished vowel counter faster than intended

---
+++ main.js
@@ -0,0 +1,22 @@
+const inputEl = document.getElementById("input-el")
+const countBtn = document.getElementById("count-btn")
+const numVowelsEl = document.getElementById("num-vowels-el")
+const vowels = ["a", "e", "i", "o", "u"]
+
+vowelCount = 0
+
+countBtn.addEventListener("click", function(){ 
+    let sentenceString = inputEl.value  
+    for (let i = 0; i<sentenceString.length; i++){
+        vowelCheck(sentenceString.charAt(i))
+    }
+})
+
+function vowelCheck(letter){
+    for (let i = 0; i < vowels.length; i++){
+        if (vowels[i] === letter){
+            vowelCount ++
+            numVowelsEl.innerText = vowelCount
+        }
+    }
+}
